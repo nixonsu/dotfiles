3c4e080540c091529889895bca548f94ecdb37e6
ni-xon <nixon.s.uch@gmail.com>
2023-04-27 22:58:48 +1000

    feat: add tmux configuration

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ ln -sf $DEV/dotfiles/.vimrc ~/.vimrc
 ln -sf $DEV/dotfiles/.ideavimrc ~/.ideavimrc
 ln -sf $DEV/dotfiles/.gitconfig ~/.gitconfig
 ln -sf $DEV/dotfiles/.zshrc ~/.zshrc
+ln -sf $DEV/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf $DEV/colorschemes/vim/monokai.vim ~/.vim/colors/monokai.vim
 echo 'Done.'
 
