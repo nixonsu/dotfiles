ce75bdf767d561f0e7329be58a1dbe0c3b06c685
ni-xon <nixon.s.uch@gmail.com>
2023-08-30 10:30:23 +1000

    fix: update open intellij alias to match toolbox version 2

--- oh-my-zsh/custom/misc.zsh
+++ oh-my-zsh/custom/misc.zsh
@@ -2,7 +2,7 @@ function find_solution_or_project {
     echo $(find . -name '*.sln' -o -name '*.csproj' -maxdepth 1 | head -n 1)
 }
 
-alias ideaj='open -a "IntelliJ IDEA" .'
+alias ideaj='open -a "IntelliJ IDEA Ultimate" .'
 alias idear='open -a "Rider" --args $(find_solution_or_project)'
 
 # sleep macos
