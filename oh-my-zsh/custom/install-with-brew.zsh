66b058bac4594072f2e04c0a1f52c7a6e96998fa
nixonsu <nixon.s.uch@gmail.com>
2024-11-08 15:39:14 +1100

    feat: add more programs to install

--- oh-my-zsh/custom/install-with-brew.zsh
+++ oh-my-zsh/custom/install-with-brew.zsh
@@ -1,2 +1,2 @@
-alias installwithbrew='echo "Installing dependencies via homebrew...";brew install git-delta tmux neovim alacritty fzf jesseduffield/lazygit/lazygit karabiner-elements lazydocker;echo "Done."'
+alias installwithbrew='echo "Installing dependencies via homebrew...";brew install git-delta tmux neovim alacritty fzf jesseduffield/lazygit/lazygit karabiner-elements lazydocker k9s shottr scroll-reverser obsidian;echo "Done."'
 
