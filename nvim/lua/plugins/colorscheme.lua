12029613731a60b71381fc83f1c332a91bcd4300
nixonsu <nixon.s.uch@gmail.com>
2025-05-18 15:24:44 +1000

    feat: load colorscheme upon start

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,16 @@
 return {
-  "catppuccin",
-  lazy = false,
-  opts = {
-    transparent_background = true,
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+    opts = {
+      transparent_background = true,
+    },
+  }, -- Keep this here so LazyVim loads this colorscheme upon start
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-mocha",
+    },
   },
 }
