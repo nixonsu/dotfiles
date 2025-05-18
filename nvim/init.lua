905839554e034aaff1b8e6273758147a747e097d
nixonsu <nixon.s.uch@gmail.com>
2025-05-18 15:29:01 +1000

    feat: nvim use vimrc

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- only source if ~/.vimrc actually exists
+if vim.fn.filereadable(vim.fn.expand("~/.vimrc")) == 1 then
+  vim.cmd("source " .. vim.fn.expand("~/.vimrc"))
+end
