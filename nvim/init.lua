1d4668f5c307103a7df6fe58feabe341bda9718e
nixonsu <nixon.s.uch@gmail.com>
2025-08-10 23:25:39 +1000

    feat: bind keymaps for lsp go to functions

--- nvim/init.lua
+++ nvim/init.lua
@@ -34,6 +34,17 @@ require("nvim-treesitter.configs").setup {
 	modules = {},
 	ignore_install = {}
 }
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(ev)
+    local opts = { buffer = ev.buf, silent = true }
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "K",  vim.lsp.buf.hover, opts)
+  end,
+})
 ------------------------------PLUGINS-------------------------------
 
 ------------------------------SETTINGS------------------------------
