3c4054ef30ef952764c433884fd85f031ddd8757
nixonsu <nixon.s.uch@gmail.com>
2025-08-10 10:05:27 +1000

    chore: delete lazyvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,1 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
 
--- only source if ~/.vimrc actually exists
-if vim.fn.filereadable(vim.fn.expand("~/.vimrc")) == 1 then
-  vim.cmd("source " .. vim.fn.expand("~/.vimrc"))
-end
