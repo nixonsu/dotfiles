ca9b68bfd5c785e6715b97fe733e571af05befa1
nixonsu <nixon.s.uch@gmail.com>
2025-09-09 13:48:07 +1000

    feat: add tmux configs and scripts

---
+++ scripts/install-oh-my-zsh-themes.zsh
@@ -0,0 +1,10 @@
+function installp10k {
+  local p10k_dir="$HOME/.oh-my-zsh/custom/themes/powerlevel10k"
+  if [ -d "$p10k_dir" ]; then
+    echo "Powerlevel10k directory already exists. Skipping clone."
+  else
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$p10k_dir"
+  fi
+}
+
+alias installp10k='echo "Installing Powerlevel10k theme...";installp10k;echo "Done."'
