f28a27cd2643408868cc30f79dbc53050f6e9a22
nixonsu <nixon.s.uch@gmail.com>
2026-04-11 21:05:05 +1000

    feat: update windows profile script

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,22 @@
+# Source private profile
+$privateProfile = Join-Path $HOME/Documents/WindowsPowerShell private_profile.ps1
+
+if (Test-Path $privateProfile) {
+    . $privateProfile
+}
+
+# Aliases
+function lg { lazygit }
+
 function df {
     & "C:\Program Files\Git\cmd\git.exe" --git-dir=$HOME/.dotfiles --work-tree=$HOME @args
 }
+
+function lgdf {
+    lazygit --git-dir=$HOME/.dotfiles --work-tree=$HOME
+}
+
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
+
+# Public/profile-managed config here
+
